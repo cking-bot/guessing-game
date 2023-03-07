463d7335db8b9f7bf8b7ae0b6cedc38f01ee5768
itscharlzard <kingcharlie1103@yahoo.com>
2023-03-07 15:13:31 -0800

    created login function

---
+++ login.py
@@ -0,0 +1,9 @@
+import model
+
+def login():
+    username = input("Please enter your username: ")
+    user_info = model.session.query(model.UserData).filter_by(username=username).first()
+    password = input("Please enter your password: ")  
+    if password != user_info.password:
+        print("Incorrect credentials.")
+        return False
